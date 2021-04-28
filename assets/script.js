066a56f0491df67498827e33979464f02f1a2178
Nick Strong <nstrong92@yahoo.com>
2021-04-28 16:12:26 -0600

    adjusted html and css

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,25 @@
-//Declare variables
+// test
+console.log("Hello!");
 
-var currentDay;
-var timeOfDay;
-var eventSchdule;
-var past;
-var present;
-var future;
+// declare variables
 
-//displays time in element with current day id
-document.getElementById("currentDay").textContent = Date();
+var scheduleTime = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5"];
+// var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
+
+//displays time in element with current day id, need to condense down to day, using moment
+var currentTime = (document.getElementById("currentDay").textContent = Date());
+
+//displays time in element with current day id, need to condense down to day, using moment
+// document.getElementById("currentDay").textContent = Date();
+
+// use jQuery
+
+//color code timeblocks based on past, present or future
+function getStyleSheet() {}
+
+// save event entered into timeblock when button is clicked (text area)
+// make sure saved information is in local storage when page is refreshed
+function saveEvent() {
+  // event needs to be saved in local storage
+  localStorage.setItem();
+}
