0bd6e9bc58a6e6818ace54cd31216b38286f92ac
Nick Strong <nstrong92@yahoo.com>
2021-04-29 11:36:30 -0600

    added date and time using moment

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,18 @@
-// test
-console.log("Hello!");
-
 // declare variables
 
 var scheduleTime = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5"];
-// var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 
 //displays time in element with current day id, need to condense down to day, using moment
-var currentTime = (document.getElementById("currentDay").textContent = Date());
 
-//displays time in element with current day id, need to condense down to day, using moment
-// document.getElementById("currentDay").textContent = Date();
+var currentTime = moment();
+$("#currentDay").text(
+  currentTime.format("[Today is ]MMM Do, YYYY, h:mm:ss a ")
+);
 
 // use jQuery
 
 //color code timeblocks based on past, present or future
-function getStyleSheet() {}
+function timeColor() {}
 
 // save event entered into timeblock when button is clicked (text area)
 // make sure saved information is in local storage when page is refreshed
